8829022742fa621c5af98e0d68888a127f87afdb
GaziMahmudur <g.mahmudur.r@gmail.com>
2025-03-11 00:04:11 +0600

    Fix text rotation style and adjust hover effect positioning for improved layout consistency

--- template/Portfolio/script.js
+++ template/Portfolio/script.js
@@ -18,5 +18,5 @@ navlist.addEventListener("click",()=>{
 let text = document.querySelector(".text p");
 
 text.innerHTML = text.innerHTML.split("").map((char,i)=>
-    `<b style="transform:rotate(${i * 6.3}deg")>${char}</b>`
+    `<b style="transform:rotate(${i * 6.3}deg)">${char}</b>`
 ).join("");
